87a89755f937a1494c99b24844e8a04bd4fcee0c
Vorrapol <vorrapol.892@gmail.com>
2019-11-08 12:48:49 +0000

    First commit Class 3 Material 20208400

---
+++ Model_Loading_Library/include/material.h
@@ -0,0 +1,35 @@
+#include <string>
+using namespace std;//for std::
+
+class Material {
+private:
+  //Variables
+  double density;
+  string colour;
+  string name;
+  double id;
+public:
+  //Default Constructure
+  Material();
+  /*Overload Constructure
+  d = density
+  c = colour
+  n = name
+  i = id
+  */
+  Material (double d, string c, string n, double i);
+  //Destuctor
+  ~Material();
+  /*Setup Function
+  d = density
+  c = colour 
+  n = name
+  i = id*/
+  void setMaterial( double d, string c, string n, double i);
+  double getdensity();//return density
+  string getcolour();//return colour
+  string getname();//return name
+  double getid();//return id
+  };
+
+ #endif // End of material.h
