ec48487e44f1dc773de8ff8a2a9dc85a44546753
unknown <slywl3@163.com>
2019-11-23 22:44:36 +0000

    Modify class Cell

--- Model_Loading_Library/include/cell.h
+++ Model_Loading_Library/include/cell.h
@@ -4,32 +4,37 @@
 #include <vector>
 #include <string>
 #include "vector.h"
+#include "material.h"
 
 class Cell
 {
 public:
+	// constructors
+	Cell();
 	Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Material& m);
 	Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Vector& v4, Material& m);
 	Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Vector& v4, Vector& v5, Vector& v6, Vector& v7, Material& m);
-
+	// destructor
+	~Cell() = default;
+	// overload assignment operator
 	Cell& operator=(const Cell& c);
 
-	double getVolume() const;
-	double getWeight() const;
-	std::string getType() const;
-	const std::vector<double>& getGravityCenter() const;
+	double getVolume() const; // get the volume of the cell
+	double getWeight() const; // get the weight of the cell
+	std::string getType() const; //get the type of the cell
+	const std::vector<double>& getGravityCenter() const; // get the gravity center of the cell
 
 private:
-	std::vector<Vector> data;
+	std::vector<Vector> data; // store the vertex of the cell
 	std::string cellType;
 	std::vector<double> gravityCenter;
 	double volume;
 	double weight;
 	double density;
 
-	void setVolume();
-	void setWeight();
-	void setGravityCenter();
+	void setVolume(); // determine the volume
+	void setWeight(); // determine the weight
+	void setGravityCenter(); // determine the gravity center
 };
 
 #endif CELL_H
--- Model_Loading_Library/src/cell.cpp
+++ Model_Loading_Library/src/cell.cpp
@@ -1,14 +1,24 @@
-#include "cell.h"
-#include "vector.h"
 #include <vector>
 #include <string>
+#include "cell.h"
+#include "vector.h"
 
 using namespace std;
 
+// define default constructor
+Cell::Cell()
+{
+	cellType = "Invalid";
+	density = 0;
+	volume = 0;
+	weight = 0;
+}
+
+// define the constructor for the Tetrahedron
 Cell::Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Material& m)
 {
 	cellType = "Tetrahedron";
-	this->density = m.getDensity();
+	this->density = m.getdensity();
 	
 	data.push_back(v0);
 	data.push_back(v1);
@@ -20,10 +30,11 @@ Cell::Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Material& m)
 	setGravityCenter();
 }
 
+// define the constructor for the Pyramid
 Cell::Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Vector& v4, Material& m)
 {
 	cellType = "Pyramid";
-	this->density = m.getDensity();
+	this->density = m.getdensity();
 
 	data.push_back(v0);
 	data.push_back(v1);
@@ -36,10 +47,11 @@ Cell::Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Vector& v4, Material&
 	setGravityCenter();
 }
 
+// define the constructor for the Hexahedron
 Cell::Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Vector& v4, Vector& v5, Vector& v6, Vector& v7, Material& m)
 {
 	cellType = "Hexahedron";
-	this->density = m.getDensity();
+	this->density = m.getdensity();
 
 	data.push_back(v0);
 	data.push_back(v1);
@@ -57,17 +69,11 @@ Cell::Cell(Vector& v0, Vector& v1, Vector& v2, Vector& v3, Vector& v4, Vector& v
 
 Cell& Cell::operator=(const Cell& c)
 {
+	// avoid self assignment
 	if (this != &c)
 	{
 		cellType = c.cellType;
-
-		vector<Vector>::const_iterator citor;
-		vector<Vector>::iterator itor;
-		for (citor = c.data.begin(), itor = data.begin(); citor != c.data.end(); citor++, itor++)
-		{
-			*itor = *citor;
-		}
-
+		data = c.data;
 		gravityCenter = c.gravityCenter;
 		volume = c.volume;
 		weight = c.weight;
@@ -79,28 +85,28 @@ Cell& Cell::operator=(const Cell& c)
 void Cell::setVolume()
 {
 	vector<Vector>::iterator itor = data.begin();
-	if (cellType == "Tetrahedron")
+	if (cellType == "Tetrahedron") 
 	{
-		Vector v1 = *itor - *(itor + 1);
+		Vector v1 = *itor - *(itor + 1); 
 		Vector v2 = *itor - *(itor + 2);
 		Vector v3 = *itor - *(itor + 3);
 
-		Vector temp = Vector::get_cross_p(v1, v2);
-		double det = Vector::get_dot_p(temp, v3);
+		Vector temp = v1.get_cross_p(v2);
+		double det = temp.get_dot_p(v3);
 
 		volume = abs(det) / 6;
 	}
-	else if (cellType == "Pyramid")
+	else if (cellType == "Pyramid") 
 	{
 		Vector v1 = *(itor + 4) - *itor;
 		Vector v2 = *(itor + 4) - *(itor + 1);
 		Vector v3 = *(itor + 4) - *(itor + 2);
 		Vector v4 = *(itor + 4) - *(itor + 3);
 
-		Vector temp1 = Vector::get_cross_p(v1, v2);
-		double det1 = abs(Vector::get_dot_p(temp1, v3));
-		Vector temp2 = Vector::get_cross_p(v1, v3);
-		double det2 = abs(Vector::get_dot_p(temp2, v4));
+		Vector temp1 = v1.get_cross_p(v2);
+		double det1 = abs(temp1.get_dot_p(v3));
+		Vector temp2 = v1.get_cross_p(v3);
+		double det2 = abs(temp2.get_dot_p(v4));
 
 		volume = (det1 + det2) / 6;
 	}
@@ -114,12 +120,12 @@ void Cell::setVolume()
 		Vector v6 = *(itor + 3) - *itor;
 		Vector v7 = *(itor + 7) - *(itor + 2);
 
-		Vector temp1 = Vector::get_cross_p(v1, v2);
-		double det1 = abs(Vector::get_dot_p(temp1, v3));
-		Vector temp2 = Vector::get_cross_p(v1, v4);
-		double det2 = abs(Vector::get_dot_p(temp2, v5));
-		Vector temp3 = Vector::get_cross_p(v1, v6);
-		double det3 = abs(Vector::get_dot_p(temp3, v7));
+		Vector temp1 = v1.get_cross_p(v2);
+		double det1 = abs(temp1.get_dot_p(v3));
+		Vector temp2 = v1.get_cross_p(v4);
+		double det2 = abs(temp2.get_dot_p(v5));
+		Vector temp3 = v1.get_cross_p(v6);
+		double det3 = abs(temp3.get_dot_p(v7));
 
 		volume = (det1 + det2 + det3) / 6;
 	}
@@ -132,7 +138,7 @@ void Cell::setWeight()
 
 void Cell::setGravityCenter()
 {
-	vector<Vector>::const_iterator itor;
+	vector<Vector>::iterator itor;
 	double x, y, z;
 	double sumX = 0, sumY = 0, sumZ = 0;
 
