c9061bd1b97a7339e6670cb56fdcbf79a566f250
Ewan Drever-Smith <eeyerd@nottingham.ac.uk>
2019-11-26 11:12:27 +0000

    Changed testfile to ensure file path is relative to directory

--- Model_Loading_Library/test/main.cpp
+++ Model_Loading_Library/test/main.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 int main()
 {
-    Model myModel("C:/Users/Ewan/Documents/.University/Computing Project/Group32/Model_Loading_Library/ExampleModel1.mod");
+    Model myModel("../test/ExampleModel1.mod");
     myModel.loadModel();
     myModel.displayVertices();
+
 }
