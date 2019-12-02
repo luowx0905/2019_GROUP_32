a257513cb817352a999b1fbeb1b3ccf245434dfa
unknown <slywl3@163.com>
2019-12-02 00:19:29 +0000

    Add inverse matrix in class Matrix

--- Model_Loading_Library/src/matrix.cpp
+++ Model_Loading_Library/src/matrix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept>
+#include <cmath>
 #include "matrix.h"
 
 using namespace std;
@@ -245,18 +246,19 @@ double Matrix::det() const
 		}
 		else
 		{
+			Matrix copy(*this);
 			double result = 1;
-			bool nonZero = false;
 			int swap = 0;
 
 			for (size_t i = 0; i < row - 1; i++)
 			{
 				size_t next;
-				if (data[i][i] == 0)
+				bool nonZero = false;
+				if (copy.data[i][i] == 0)
 				{
 					for (next = i + 1; next < row; next++)
 					{
-						if (data[i][i] != 0)
+						if (copy.data[i][i] != 0)
 						{
 							nonZero = true;
 							break;
@@ -271,25 +273,25 @@ double Matrix::det() const
 					swap++;
 					for (size_t j = 0; j < col; j++)
 					{
-						double temp = data[i][j];
-						data[i][j] = data[next][j];
-						data[next][j] = temp;
+						double temp = copy.data[i][j];
+						copy.data[i][j] = copy.data[next][j];
+						copy.data[next][j] = temp;
 					}
 				}
 
 				for (next = i + 1; next < row; next++)
 				{
-					double factor = data[next][i] / data[i][i];
+					double factor = copy.data[next][i] / copy.data[i][i];
 					for (size_t k = 0; k < row; k++)
 					{
-						data[next][k] += -factor * data[i][k];
+						copy.data[next][k] += -factor * copy.data[i][k];
 					}
 				}
 			}
 
 			for (size_t i = 0; i < row; i++)
 			{
-				result *= data[i][i];
+				result *= copy.data[i][i];
 			}
 
 			if (swap % 2 == 0)
@@ -305,6 +307,25 @@ double Matrix::det() const
 	else
 	{
 		throw runtime_error("The determinant is invalid");
+	}
+}
+
+Matrix Matrix::inv()
+{
+	if (row != col)
+	{
+		throw runtime_error("No inverse matrix");
+	}
+
+	double determinant = this->det();
+	if (determinant != 0)
+	{
+		Matrix temp = this->adjugate().scale(1 / determinant);
+		return temp;
+	}
+	else
+	{
+		throw runtime_error("No inverse matrix");
 	}
 }
 
@@ -335,4 +356,107 @@ int Matrix::getCol() const
 int Matrix::getSize() const
 {
 	return size;
+}
+
+Matrix Matrix::scale(double size)
+{
+	Matrix temp(*this);
+
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < col; j++)
+		{
+			temp.data[i][j] = data[i][j] * size;
+		}
+	}
+
+	return temp;
+}
+
+Matrix Matrix::subMat(int rowStart, int colStart, int rowEnd, int colEnd)
+{
+	if (rowEnd == 0 && colEnd == 0)
+	{
+		Matrix temp(row - rowStart, col - colStart);
+
+		for (size_t i = rowStart, m = 0; i < row; i++, m++)
+		{
+			for (size_t j = colStart, n = 0; j < col; j++, n++)
+			{
+				temp.data[m][n] = data[i][j];
+			}
+		}
+
+		return temp;
+	}
+	else if (rowStart <= rowEnd && colStart <= colEnd && rowEnd < row && colEnd < col)
+	{
+		Matrix temp(rowEnd - rowStart + 1, colEnd - colStart + 1);
+
+		for (size_t i = rowStart, m = 0; i <= rowEnd; i++, m++)
+		{
+			for (size_t j = colStart, n = 0; j <= colEnd; j++, n++)
+			{
+				temp.data[m][n] = data[i][j];
+			}
+		}
+
+		return temp;
+	}
+	else
+	{
+		throw runtime_error("Submatrix size error");
+	}
+}
+
+Matrix Matrix::minor(int row, int col)
+{
+	Matrix temp(this->row - 1, this->col - 1);
+
+	for (size_t i = 0, m = 0; i < this->row; i++)
+	{
+		if (i == row)
+		{
+			continue;
+		}
+		for (size_t j = 0, n = 0; j < this->col; j++)
+		{
+			if (j == col)
+			{
+				continue;
+			}
+
+			temp.data[m][n] = data[i][j];
+			n++;
+		}
+		m++;
+	}
+
+	return temp;
+}
+
+Matrix Matrix::cofactor(int row, int col)
+{
+	Matrix temp = this->minor(row, col);
+
+	temp = temp.scale(pow(-1, row + col));
+
+	return temp;
+}
+
+Matrix Matrix::adjugate()
+{
+	Matrix result(row, col);
+
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < col; j++)
+		{
+			Matrix temp = this->minor(i, j);
+			double determinant = temp.det() * pow(-1, i + j);
+			result.data[j][i] = determinant;
+		}
+	}
+
+	return result;
 }
