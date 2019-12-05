fa77da4994f6de25d4b99f8640db332b8d4629ae
Ewan Drever-Smith <eeyerd@nottingham.ac.uk>
2019-12-05 14:50:37 +0000

    Updated matrix test

--- Model_Loading_Library/test/matrixTestMain.cpp
+++ Model_Loading_Library/test/matrixTestMain.cpp
@@ -4,17 +4,22 @@
 using namespace std;
 int main()
 {
-  cout<<"Making a 3x3 vector"<<endl;
-  Matrix myMatrix(3,3);
-  cout<<myMatrix<<endl;
-  cout<<"Populating vector with random numbers between 0 and 9"<<endl;
-  for(int cols = 0; cols < myMatrix.getCol(); cols++)
+  cout<<"Making two 3x3 vectors"<<endl;
+  Matrix matrix1(3,3);
+  Matrix matrix2(3,3);
+  cout<<"Matrix1:\n"<<matrix1<<"\nMatrix2:\n"<<matrix2<<endl;
+  cout<<"Populating vectors with random numbers between 0 and 9"<<endl;
+  for(int cols = 0; cols < 3; cols++)
   {
-    for(int rows = 0; rows < myMatrix.getRow(); rows++)
+    for(int rows = 0; rows < 3; rows++)
     {
-      myMatrix(cols,rows) = rand()%10;
+      matrix1(cols,rows) = rand()%10;
+      matrix2(cols,rows) = rand()%10;
     }
   }
-  cout<<myMatrix<<endl;
-  cout<<"Finding the inverse of this matrix\n"<<myMatrix.inv()<<endl;
+  cout<<"Matrix1:\n"<<matrix1<<"\nMatrix2:\n"<<matrix2<<endl;
+  cout<<"Multiplying them together\n"<<matrix1*matrix2<<endl;
+  cout<<"Adding them together\n"<<matrix1+matrix2<<endl;
+  cout<<"Subtracting matrix2 from matrix1\n"<<matrix1-matrix2<<endl;
+  cout<<"Finding the inverse of matrix1\n"<<matrix1.inv()<<endl;
 }
