d70c05434e255063a6157234824e36e0e64de8bf
Ewan Drever-Smith <eeyerd@nottingham.ac.uk>
2019-12-05 10:04:09 +0000

    Added first doxygen comments

--- Model_Loading_Library/include/model.h
+++ Model_Loading_Library/include/model.h
@@ -1,6 +1,9 @@
+// model.h
 #ifndef MODEL_H_INCLUDED
 #define MODEL_H_INCLUDED
-
+/** @file
+ * This file contains the declarations for the model class
+ */
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -15,24 +18,27 @@ using namespace std;
 class Model
 {
     public:
-    Model(string filePath); 
-    Model();
-    ~Model();
-    Model(const Model&);
-    const Model& operator=(const Model&);
+    Model(string filePath); //Constructor that takes the file path to the model file as an argument and loads its data into the object
+    Model(); //default constructor
+    ~Model(); //Destructor deallocates memory associated with vector lists
+    Model(const Model&); //copy constructor
+    const Model& operator=(const Model&); //operator= overload
 
-    void displayVertices()const;
-    void displayCells()const;
-    void displayMaterials()const;
+    void displayVertices()const;//outputs to the console each vertice and its coordinate vector (x,y,z)
+    void displayCells()const;//outputs to the console each cell and its properties (type, volume, weight, density)
+    void displayMaterials()const;//outputs to the console each material in the model and its properties (Density, name colour)
 
     Vector getModelCentre()const;//Returns models centre as a vector point. still TODO
-    double getModelWeight()const;
-    long getNumberOfVertices()const;
-    long getNumberOfCells()const;
-    long getNumberOfMaterials()const;
+    double getModelWeight()const;//returns the combined weight of all cells
+    long getNumberOfVertices()const;//returns the length of listOfVectors
+    long getNumberOfCells()const;//returns the length of listOfCells
+    long getNumberOfMaterials()const;//returns the length of listOfMaterials
 
     
     private:
+    //Note that each object in the object lists is stored at the index of its ID - This was done for speed in locating the object needed however it does have consequences
+    //in that every ID in the file must be consecutive or else the object will attempted to be added to a point outside the range of the std::vector
+    //In future if this is no longer acceptable, then store ID as a parameter of the object itself. This would mean that each object could be stored at any point in the list, but would take longer to search for the object with a particular ID.
     vector<Vector> listOfVectors;
     vector<Cell> listOfCells;
     vector<Material> listOfMaterials;
@@ -43,12 +49,12 @@ class Model
     */
     //each column pertains to a new cellID
     void generateCellList(int cellListLength); //function populates a 2D vector with all the vectors each cell should contain. Will then be used to generate list containing actual Cell objects once the entire file has been read
-    void readVector(string line);
-    void readCell(string line);
-    void readMaterial(string line);
-    void setFilePath(string fp);
-    void loadModel();
-    string sourceFilePath;
-    ifstream fileStream;
+    void readVector(string line);//reads the passed line string and constructs a vector object using details from the line. This object is then added to the vector list at the index of its ID
+    void readCell(string line);//reads the the passed line string and adds the vector 'recipe' to the uninitCellList to be constructed at a later time.
+    void readMaterial(string line);//reads the passed line string and constructs a Material object using details from the line. This object is then added to the material list at the index of its ID
+    void setFilePath(string fp);//Unused - delete?
+    void loadModel();//Loads file located at sourceFilePath into model object
+    string sourceFilePath; // contains the location of the file to be read.
+    ifstream fileStream;//TODO remove this from the class data and use locally inside loadModel()
 };
 #endif // MODEL_H_INCLUDED
