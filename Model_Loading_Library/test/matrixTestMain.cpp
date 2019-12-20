1698b040bc12a43c43693f0a2e22658f9cb56fa9
Ewan Drever-Smith <eeyerd@nottingham.ac.uk>
2019-12-20 13:05:02 +0000

    Updated test main with new matrix functions

--- Model_Loading_Library/test/matrixTestMain.cpp
+++ Model_Loading_Library/test/matrixTestMain.cpp
@@ -1,9 +1,13 @@
+#define _USE_MATH_DEFINES
 #include "matrix.h"
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
+#include <cmath>
 using namespace std;
 int main()
 {
+  srand(time(NULL));
   cout<<"Making two 3x3 vectors"<<endl;
   Matrix matrix1(3,3);
   Matrix matrix2(3,3);
@@ -22,4 +26,13 @@ int main()
   cout<<"Adding them together\n"<<matrix1+matrix2<<endl;
   cout<<"Subtracting matrix2 from matrix1\n"<<matrix1-matrix2<<endl;
   cout<<"Finding the inverse of matrix1\n"<<matrix1.inv()<<endl;
+
+  Vector vector1(1,-5,10);
+  cout<<"Vector1:\n"<<vector1<<endl;
+  Matrix zRot = Matrix::genRotationMatrix('z',M_PI/2);
+  Matrix yRot = Matrix::genRotationMatrix('y',M_PI/2);
+  Matrix xRot = Matrix::genRotationMatrix('x',M_PI/2);
+  cout<<"Vector1 rotated by pi/2 rads in the z plane is:\n"<< (Matrix(vector1)*zRot).convertToVector() << endl;
+  cout<<"Vector1 rotated by pi/2 rads in the y plane is:\n"<< (Matrix(vector1)*yRot).convertToVector() << endl;
+  cout<<"Vector1 rotated by pi/2 rads in the x plane is:\n"<< (Matrix(vector1)*xRot).convertToVector() << endl;
 }
