7bdc1d4116512887b25511fa8567ef18a7ecea4b
Ewan Drever-Smith <eeyerd@nottingham.ac.uk>
2020-04-01 14:25:04 +0100

    Updated vectorTest

--- Model_Loading_Library/test/vectorTest.cpp
+++ Model_Loading_Library/test/vectorTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "vector.h"
+#include <sstream>
 int main(int argc, char *argv[])
 {
     //initialise some vectors to use for testing
@@ -18,12 +19,12 @@ int main(int argc, char *argv[])
                             && testVector.get_j() == 9
                             && testVector.get_k() == 7) ? 0 : 1;
     //Add test
-    Vector temp = testVector2 + testVector3;
-    bool ADD_STATUS = (temp.get_i() == 14
-                    && temp.get_j() == 15
-                    && temp.get_k() == 11) ? 0 : 1;
+    Vector temp1 = testVector2 + testVector3;
+    bool ADD_STATUS = (temp1.get_i() == 14
+                    && temp1.get_j() == 15
+                    && temp1.get_k() == 11) ? 0 : 1;
     //operator== test
-    bool EQUALITY_OPERATOR_STATUS = (!(testVector2 == testVector3) && testVector3 == Vector(6,9,7) ) ? 0 : 1;
+    bool EQUALITY_OP_STATUS = (!(testVector2 == testVector3) && testVector3 == Vector(6,9,7) ) ? 0 : 1;
     
     //get_got_p() test
     bool DOT_PROD_STATUS = (testVector2.get_dot_p(testVector3) == 130) ? 0 : 1;
@@ -31,7 +32,13 @@ int main(int argc, char *argv[])
     //get_cross_p() test
     bool CROSS_PROD_STATUS = (testVector2.get_cross_p(testVector3) == Vector(6,-32,36)) ? 0 : 1;
 
-    return (GET_MAG_STATUS  == 0 && SET_AND_GET_STATUS == 0 && 
-            ADD_STATUS      == 0 && EQUALITY_OPERATOR_STATUS == 0 && 
-            DOT_PROD_STATUS == 0 && CROSS_PROD_STATUS == 0) ? 0 : 1; //1 represents error
+    //operator<< test
+    std::ostringstream temp2;
+    temp2 << testVector2;
+    bool OUTPUT_OP_STATUS = (temp2.str()=="(8, 6, 4)") ? 0 : 1;
+
+    return (GET_MAG_STATUS   == 0 && SET_AND_GET_STATUS == 0 && 
+            ADD_STATUS       == 0 && EQUALITY_OP_STATUS == 0 && 
+            DOT_PROD_STATUS  == 0 && CROSS_PROD_STATUS  == 0 &&
+            OUTPUT_OP_STATUS == 0) ? 0 : 1; //1 represents error
 }
